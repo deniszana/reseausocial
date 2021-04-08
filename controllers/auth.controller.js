2bbd61e448f794ff4c2c29b2eebf6b65c0dc89f8
zana <denis.zana@gmail.com>
2021-04-08 14:37:04 +0200

    api user register correction

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -14,7 +14,7 @@ const createToken = (id) => {
 module.exports.signUp = async (req, res) => {
   const {pseudo, email, password} = req.body
 
-  try { auth
+  try { 
     const user = await UserModel.create({pseudo, email, password });
     res.status(201).json({ user: user._id});
   }
