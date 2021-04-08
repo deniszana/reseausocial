e9c67a866c8211d1f93301e4fdb7e697199f1555
zana <denis.zana@gmail.com>
2021-04-08 17:27:41 +0200

    upload images

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 const postController = require('../controllers/post.controller');
-
+const uploadController = require('../controllers/upload.controller');
+const multer = require("multer");
+const upload = multer();
 
 router.get('/', postController.readPost);
-router.post('/',  postController.createPost);
+router.post('/',  upload.single("file"), postController.createPost);
 
 router.put('/:id', postController.updatePost);
 router.delete('/:id', postController.deletePost);
