87ac81aa563e411b1e85ee7a84e4b809ad56c637
zana <denis.zana@gmail.com>
2021-04-08 17:53:05 +0200

    cors

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-
+const userRoutes = require('./routes/user.routes');
+const postRoutes = require('./routes/post.routes');
 
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const {checkUser, requireAuth} = require('./middleware/auth.middleware');
-
-const userRoutes = require('./routes/user.routes');
-const postRoutes = require('./routes/post.routes');
-
+const cors = require('cors');
 const app = express();
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['sessionId'],
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  'preflightContinue': false
+}
+app.use(cors(corsOptions));
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
