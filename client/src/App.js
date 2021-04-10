6d376f5f6010cd07dacb135faebbef3281b66258
zana <denis.zana@gmail.com>
2021-04-10 16:01:28 +0200

    Client profil SignIn SignOut

---
+++ client/src/App.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import Routes  from './components/Routes';
+import axios from "axios";
+import { UidContext } from "./components/AppContext";
+
+
+
+const App = () => {
+
+  const [uid, setUid] = useState(null);
+
+useEffect(() => {
+  const fetchToken = async () => {
+    await axios({
+      method: "get",
+      url: `${process.env.REACT_APP_API_URL}jwtid`,
+      withCredentials: true,
+    })
+      .then((res) => {
+        setUid(res.data);
+      })
+      .catch((err) => console.log("No token"));
+  };
+  fetchToken();
+
+  }, [uid]);
+
+
+  return (
+    <UidContext.Provider value={uid}>
+      <Routes />
+    </UidContext.Provider>
+  );
+};
+
+export default App;
---
+++ client/src/components/Routes/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import Home from '../../pages/Home';
+import Profil from '../../pages/Profil';
+import Trending from '../../pages/Trending';
+
+const index = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={Home}></Route>
+                <Route path="/profil" exact component={Profil}></Route>
+                <Route path="/trending" exact component={Trending}></Route>
+                <Redirect to="/"></Redirect>
+            </Switch>
+        </Router>
+    );
+};
+
+export default index;
